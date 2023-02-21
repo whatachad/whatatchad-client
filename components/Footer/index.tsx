111c747737d789961d48ba61fc35422d0b2d1cf9
Brandon Ha <idevbrandon@gmail.com>
2023-02-21 18:55:31 +0900

    design : header & footer component for main 3 pages

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,7 +4,7 @@ import { FooterContainer } from "./styles";
 const Footer = () => {
   return (
     <FooterContainer>
-      <h1>What a Chad</h1>
+      <h1>What a Chad 🔱</h1>
     </FooterContainer>
   );
 };
