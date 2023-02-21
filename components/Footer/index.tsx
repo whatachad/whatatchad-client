318fe91d7f10b47e16cc7446f126c158f955c3f0
Brandon Ha <idevbrandon@gmail.com>
2023-02-21 19:55:51 +0900

    design : Add a github link on footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { FooterContainer } from "./styles";
+import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <FooterContainer>
       <h1>What a Chad 🔱</h1>
+      <a href="https://github.com/whatachad">
+        <BsGithub /> All rights reserved.
+      </a>
     </FooterContainer>
   );
 };
