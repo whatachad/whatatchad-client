7e7b0063203280075f17a19f3ec7e2b841a41543
Brandon Ha <idevbrandon@gmail.com>
2023-04-08 17:50:44 +0900

    Render conditionally

--- redux/actions/user.ts
+++ redux/actions/user.ts
@@ -1,6 +1,11 @@
-import { SignupData } from "@/types/users";
+import { LoginData, SignupData } from "@/types/users";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../utils/api";
+
+export interface Tokens {
+  accessToken: string;
+  refreshToken: string;
+}
 
 // 회원가입
 export const signup = createAsyncThunk(
@@ -18,7 +23,18 @@ export const signup = createAsyncThunk(
 );
 
 // 로그인
-export const login = createAsyncThunk("user/login", async (data: any) => {
-  try {
-  } catch (error: any) {}
-});
+export const login = createAsyncThunk(
+  "user/login",
+  async (data: LoginData, thunkAPI) => {
+    try {
+      const response = await axios.post("/login", data);
+      const tokens: Tokens = response.data;
+
+      localStorage.setItem("accessToken", tokens.accessToken);
+      localStorage.setItem("refreshToken", tokens.refreshToken);
+      return response.data;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
